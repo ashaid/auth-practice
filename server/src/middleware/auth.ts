94f18445662548740f250af490384bb2902952ab
tony <anthonyshaidaee@gmail.com>
2025-03-04 18:47:10 -0600

    added auth middleware function to check jwt token validity

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+  user?: { id: string };
+}
+
+export const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "my_super_secret_jwt_secret"
+    );
+    req.user = decoded as { id: string };
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
